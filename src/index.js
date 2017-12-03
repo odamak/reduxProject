63da40a532e3a8da5d7aa3538ccd70adcc7d66d3
omar <omardamak1@gmail.com>
2017-12-03 21:55:52 +0000

    Simplify App.js removing content and install react-redux and add Provider

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 import { createStore } from 'redux'
 import reducer from './reducers'
+import { Provider } from 'react-redux'
 
 //const store = createStore(reducer)
 const store = createStore(
@@ -15,5 +16,9 @@ const store = createStore(
 
 //console.log(store)
 
-ReactDOM.render(<App store={store} />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(
+    <Provider store={store}>
+        <App/>
+    </Provider>, document.getElementById('root'))
+
+registerServiceWorker()
