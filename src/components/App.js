7b5f9f2c11f53b0f2a1c877bc89b1e7547ce9afb
omar <omardamak1@gmail.com>
2017-12-16 18:15:01 +0000

    Add "connect" to the "App" component, retrieve "calendar" object from store and transform to array

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux'
 
 class App extends Component {
 
   render() {
+    console.log('Props',this.props)
     return (
       <div>
         Hello world
@@ -11,4 +13,23 @@ class App extends Component {
   }
 }
 
-export default App;
+function mapStateToProps (calendar) {
+    const dayOrder = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday']
+
+    return {
+        calendar: dayOrder.map((day) => ({
+            day,
+            meals: Object.keys(calendar[day]).reduce((meals, meal) => {
+                meals[meal] = calendar[day][meal]
+                ? calendar[day][meal]
+                : null
+
+            return meals
+            }, {})
+        }) )
+    }
+}
+
+
+//export default App;
+export default connect(mapStateToProps)(App)
