752a5b93c09eb837f1caee0d1e20015dc293ab34
omar <omardamak1@gmail.com>
2017-12-17 18:57:51 +0000

    Add middleware log

--- src/index.js
+++ src/index.js
@@ -1,25 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-// import App from './App';
-import App from './components/App';
-import registerServiceWorker from './registerServiceWorker';
-import { createStore } from 'redux'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './components/App'
+import registerServiceWorker from './registerServiceWorker'
+import { createStore, applyMiddleware, compose } from 'redux'
 import reducer from './reducers'
 import { Provider } from 'react-redux'
 
-//const store = createStore(reducer)
+const logger = store => next => action => {
+  console.group(action.type)
+  console.info('dispatching', action)
+  let result = next(action)
+  console.log('next state', store.getState())
+  console.groupEnd(action.type)
+  return result
+}
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+
 const store = createStore(
-    reducer,
-    //if windows blablabla lives in our window object, then go ahead and invoke that
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  reducer,
+  composeEnhancers(
+    applyMiddleware(logger)
+  )
 )
 
-//console.log(store)
-
 ReactDOM.render(
-    <Provider store={store}>
-        <App/>
-    </Provider>, document.getElementById('root'))
-
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+)
 registerServiceWorker()
